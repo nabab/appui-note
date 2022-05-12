1b4e7854d83e2df559b821587eaaafe1442d72b0
nabab <thomas.nabet@gmail.com>
2022-05-12 14:23:47 +0200

    Fix in request order for creation

--- src/mvc/html/table_notes.php
+++ src/mvc/html/table_notes.php
@@ -13,7 +13,11 @@
                      icon: 'nf nf-fa-plus',
                      notext: false,
                      action: markdown
-                     }]">
+                     }]"
+           :order="[{
+                   field: 'last_edit',
+                   dir: 'DESC'
+                   }]">
   <bbns-column field="id_note"
                :editable="false"
                :hidden="true"/>
@@ -28,7 +32,7 @@
                :editable="false"
                :width="50"
                cls="bbn-c"/>
-  
+
   <bbns-column title="<?= _("Resume") ?>"
                :editable="false"
                :render="getResume"/>
@@ -82,58 +86,58 @@
 </bbn-table>
 
 <script type="text/x-template" id="apst-notes-content">
-  <div style="height: 300px; margin-right:30px; position:relative">
-    <input type="hidden" :value="source.id_notes">
-    <div  v-html="source.content ? source.content : 'no content'" style="height:100%;
-    width:100%"></div>
+<div style="height: 300px; margin-right:30px; position:relative">
+  <input type="hidden" :value="source.id_notes">
+  <div  v-html="source.content ? source.content : 'no content'" style="height:100%;
+                                                                       width:100%"></div>
   </div>
 </script>
 
 <script type="text/x-template" id="apst-new-note">
-  <bbn-form :data="source"
-            ref="form"
-            confirm-leave="<?=_("Are you sure you want to leave this form without saving your changes?")?>"
-            :action="'adherent/actions/notes/' + ( source.id ? 'update' : 'insert')"
-            :buttons="['submit', 'cancel', 'close']"
-            @success="success"
-            @failure="failure"
->
-    <div class="bbn-padded bbn-w-100" style="min-height: 500px">
+<bbn-form :data="source"
+          ref="form"
+          confirm-leave="<?=_("Are you sure you want to leave this form without saving your changes?")?>"
+          :action="'adherent/actions/notes/' + ( source.id ? 'update' : 'insert')"
+          :buttons="['submit', 'cancel', 'close']"
+          @success="success"
+          @failure="failure"
+          >
+  <div class="bbn-padded bbn-w-100" style="min-height: 500px">
 
 
-      <label class="bbn-form-label">
-        <?=_("Category")?>
-      </label>
-      <div class="bbn-form-field">
-        <bbn-dropdown name="id_type_note"
-                      required="required"
-                      v-model="source.id_type_note"
-                      :source="options.notes"
-        ></bbn-dropdown>
-      </div>
+    <label class="bbn-form-label">
+      <?=_("Category")?>
+  </label>
+    <div class="bbn-form-field">
+      <bbn-dropdown name="id_type_note"
+                    required="required"
+                    v-model="source.id_type_note"
+                    :source="options.notes"
+                    ></bbn-dropdown>
+  </div>
 
-      <label class="bbn-form-label">
-        <?=_("Text")?>
-      </label>
-      <div class="bbn-form-field">
-        <bbn-rte v-model="source.texte"
-                 name="texte"
-                 required="required"
-                 class="bbn-100"
-        >
-        </bbn-rte>
-      </div>
+    <label class="bbn-form-label">
+      <?=_("Text")?>
+  </label>
+    <div class="bbn-form-field">
+      <bbn-rte v-model="source.texte"
+               name="texte"
+               required="required"
+               class="bbn-100"
+               >
+  </bbn-rte>
+  </div>
 
-      <label class="bbn-form-label" v-if="isOwner">
-        <?=_("Blocked")?>
-      </label>
-      <div class="bbn-form-field" v-if="isOwner">
-        <bbn-checkbox v-model="source.confidentiel"
-                      name="confidentiel"
-                      id="t4Wo9nnuXZb4wXifPahWB"
-                      :checked="source.confidentiel ? true : false"
-        ></bbn-checkbox>
-      </div>
-    </div>
+    <label class="bbn-form-label" v-if="isOwner">
+      <?=_("Blocked")?>
+  </label>
+    <div class="bbn-form-field" v-if="isOwner">
+      <bbn-checkbox v-model="source.confidentiel"
+                    name="confidentiel"
+                    id="t4Wo9nnuXZb4wXifPahWB"
+                    :checked="source.confidentiel ? true : false"
+                    ></bbn-checkbox>
+  </div>
+  </div>
   </bbn-form>
 </script>
