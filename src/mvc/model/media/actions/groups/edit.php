15ca67b1130bf00e9cf8d2688aac245246fd8cc6
Loredana Bruno <loredana@bbn.so>
2022-07-26 20:35:22 +0200

    insert media group link

--- src/mvc/model/media/actions/groups/edit.php
+++ src/mvc/model/media/actions/groups/edit.php
@@ -1,19 +1,25 @@
 <?php
-
-
 if ($model->hasData(['id', 'file'], true)) {
   $medias = new bbn\Appui\Medias($model->db);
+	$cms = new \bbn\Appui\Cms($model->db);
+
 	$ok = 0;
-	
 	if(($name = $model->data['file'][0]['name']) && ($title = $model->data['file'][0]['title'])){
 		$ok += count($medias->update($model->data['id'], $name, $title));
 	}
-	if(!empty($model->data['linkUrl'])){
-		
-		//here update the url of the media!
+	if($model->hasData(['idGroup']) && $model->data['file'][0]['link'] && \bbn\Str::isUrl($model->data['file'][0]['link'] )){
+
+		$ok += $model->db->update('bbn_medias_groups_medias', [
+			'link' => $model->data['file'][0]['link'] 
+		],[
+			'id_media' => $model->data['id'],
+			'id_group' => $model->data['idGroup']
+
+		]);		
 	}
 
   return [
+		'media' => $cms->getMedia($model->data['id'], true),
     'success' => !!$ok
   ];
 }
