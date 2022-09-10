e6bf35f4d1b283d338ce4db19e02ccf0077434a1
nabab <thomas.nabet@gmail.com>
2022-09-10 16:03:05 +0200

    Better image

--- src/mvc/public/media.php
+++ src/mvc/public/media.php
@@ -4,7 +4,7 @@ use bbn\X;
 use bbn\Str;
 /** @var $ctrl \bbn\Mvc\Controller */
 
-if ($ctrl->hasData('root')) {
+if (defined('BBN_BASEURL') && $ctrl->hasData('root')) {
   if (empty(BBN_BASEURL)) {
     $ctrl->setUrl($ctrl->data['root'].'media')
       ->setColor('#ccffcc', '#009688')
--- src/mvc/public/media/image/index.php
+++ src/mvc/public/media/image/index.php
@@ -1,16 +1,65 @@
 <?php
-/*
- * Describe what it does!
- *
- * @var $ctrl \bbn\Mvc\Controller
- *
- */
-if ($ctrl->hasArguments()
-  && ($id = $ctrl->arguments[0])
-	&& ($medias = new \bbn\Appui\Medias($ctrl->db))
-	&& ($media = $medias->getMedia($id, false, $ctrl->get['w'] ?? null))
-) {
-	$ctrl->setMode("image");
-  $image = new \bbn\File\Image($media);
-  $ctrl->obj->img = $image;
+use bbn\X;
+use bbn\Str;
+use bbn\File\System;
+use bbn\Appui\Medias;
+
+if ($ctrl->hasArguments()) {
+	$medias = new Medias($ctrl->db);
+	$path = '';
+  $url = $ctrl->getRequest();
+  $realUrl = $url;
+  
+  $reg = '/\.bbn-[\-\dwhc]+\./';
+  $width = $ctrl->get['w'] ?? null;
+  $height = $ctrl->get['h'] ?? null;
+  $crop = $ctrl->get['c'] ?? false;
+  if (preg_match($reg, $url, $match)) {
+    $ext1 = Str::fileExt($url, true);
+    $ext2 = Str::fileExt($ext1[0], true);
+    $realUrl = dirname($url) . '/' . $ext2[0] . '.' . $ext1[1];
+    $rules = X::split($ext2[1], '-');
+    array_shift($rules);
+    $is = null;
+    foreach ($rules as $r) {
+      switch ($r) {
+        case 'w':
+          $is = 'width';
+          break;
+        case 'h':
+          $is = 'height';
+          break;
+        case 'c':
+          $is = 'crop';
+          break;
+        default:
+          if ($is) {
+            $$is = $r ?: false;
+          }
+      }
+    }
+  }
+
+  $idMediaFromUrl = false;
+  if (Str::isUid($ctrl->arguments[0])
+    && ($media = $medias->getMedia($ctrl->arguments[0], false, $width, $height, $crop))
+  ) {
+		$path = $media['file'];
+  }
+  elseif ($idMediaFromUrl = $medias->urlToId($realUrl)) {
+    $path = $medias->getMedia($idMediaFromUrl, false, $width, $height, $crop);
+  }
+
+  if ($path && file_exists($path)) {
+    if ($idMediaFromUrl) {
+      $fs = new System();
+      $fs->createPath(dirname(BBN_PUBLIC . $url));
+      symlink(is_link($path) ? readlink($path) : $path, BBN_PUBLIC . $url);
+    }
+
+    $ctrl->setMode("image");
+    $ctrl->obj->img = $path;
+	}
 }
+
+// 404
