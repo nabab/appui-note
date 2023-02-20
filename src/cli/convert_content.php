2db7c009f49a7ce70666f7f59942a1a27e17563e
Dev Leo <dev-leo@leo.vm.m3l.co>
2023-02-20 12:14:12 +0100

    convertions script

---
+++ src/cli/convert_content.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ *  Describe what it does
+ *
+ **/
+use bbn\X;
+
+$rows = $ctrl->db->rselectAll('bbn_notes_versions', ['version', 'id_note', 'content']);
+
+foreach($rows as $row) {
+  $blocks = json_decode($row['content'], true);
+  $isChanged = false;
+  foreach($blocks as &$block) {
+    switch($block['type']) {
+      case 'button':
+      case 'text':
+        if (isset($block['text'])) {
+          $content = $block['text'];
+          unset($block['text']);
+          $block['content'] = $content;
+          $isChanged = true;
+        }
+        break;
+      case 'carousel':
+      case 'gallery':
+      case 'image':
+      case 'imagetext':
+      case 'video':
+        if (isset($block['source'])) {
+          $content = $block['source'];
+          unset($block['source']);
+          $block['content'] = $content;
+          $isChanged = true;
+        }
+        break;
+    }
+    if (isset($block['style'])) {
+      $isChanged = true;
+      foreach($block['style'] as $name => $value) {
+        $block[$name] = $value;
+      }
+      unset($block['style']);
+    }
+  }
+  if ($isChanged) {
+    X::hdump($ctrl->db->update('bbn_notes_versions', ['content' => json_encode($blocks)], ['id_note' => $row['id_note'], 'version' => $row['version']]));
+  }
+}
