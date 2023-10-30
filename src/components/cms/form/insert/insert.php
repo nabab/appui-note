822c5ae7f476cb5b9e2544d3a09cae24d099628f
Mirko Argentino <mirko@bbn.solutions>
2023-10-30 10:13:51 +0100

    Bugfixes

--- src/components/cms/form/insert/insert.php
+++ src/components/cms/form/insert/insert.php
@@ -19,13 +19,11 @@
                           :prefix="prefix"
                           v-model="formData.url"/>
 
-    <label v-if="!id_type"
-           class="bbn-b">
+    <label class="bbn-b">
       <?=_('Article type')?>
     </label>
     <div class="bbn-flex-width">
-      <bbn-dropdown v-if="!id_type"
-                    v-model="formData.type"
+      <bbn-dropdown v-model="formData.type"
                     :source="types"
                     class="bbn-flex-fill bbn-right-sspace"
                     :required="true"
