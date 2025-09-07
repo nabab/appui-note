820e6f059598eb124ec7b122579bdb79f3b2d488
nabab <thomas.nabet@gmail.com>
2025-09-07 16:47:25 +0200

    Moved languages to core

--- src/mvc/public/form.php
+++ src/mvc/public/form.php
@@ -2,5 +2,5 @@
 $ctrl->setIcon('nf nf-fa-pencil')
   ->combo(_('Notes Form'), [
     'types' => $ctrl->inc->options->textValueOptions($ctrl->inc->options->fromCode('types', 'note', 'appui')),
-    'languages' => $ctrl->inc->options->codeOptions('languages', 'i18n', 'appui')
+    'languages' => $ctrl->inc->options->codeOptions('languages', 'core', 'appui')
   ]);
